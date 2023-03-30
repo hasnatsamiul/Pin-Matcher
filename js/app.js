97fd9f96712524b0a8389959d0532ad8960c8175
Your Name <hasnatsamiul04@gmail.com>
2023-03-30 17:37:49 +0600

    Generate PIN

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
-console.log('app connected');
+function getPin() {
+    const pin = generatePin();
+    const pinString = pin + '';
+    if (pinString.length === 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
 
+}
+
+
+function generatePin() {
+    const random = Math.round(Math.random() * 10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click', function () {
+    const pin = getPin();
+    // console.log(pin);
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+})
